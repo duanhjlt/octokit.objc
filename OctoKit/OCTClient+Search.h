de51f35f1967b1563c7db4bb33acbe092c5ceab5
duanhongjin <duanhjlt@163.com>
2017-05-01 22:04:26 +0800

    Add Search Users

--- OctoKit/OCTClient+Search.h
+++ OctoKit/OCTClient+Search.h
@@ -20,6 +20,16 @@
 ///  Returns a signal which will send the search result `OCTRepositoriesSearchResult`.
 - (RACSignal *)searchRepositoriesWithQuery:(NSString *)query orderBy:(NSString *)orderBy ascending:(BOOL)ascending;
 
+///  Search users.
+///
+///  query     - The search keywords, as well as any qualifiers. This must not be nil.
+///  orderBy   - The sort field. One of stars, forks, or updated. Default: results
+///              are sorted by best match. This can be nil.
+///  ascending - The sort order, ascending or not.
+///
+///  Returns a signal which will send the search result `OCTRepositoriesSearchResult`.
+- (RACSignal *)searchUsersWithQuery:(NSString *)query orderBy:(NSString *)orderBy ascending:(BOOL)ascending;
+
 /// Search repositories.
 ///
 /// @param language
